50d80f983a71c5a2d72af433fcdf2efca7a5f7eb
Mykyta Gusak <nikitagusak4@gmail.com>
2024-02-13 12:44:22 +0100

    day page search added

--- lib/src/modules/pages/day/cubit/day_page_cubit.dart
+++ lib/src/modules/pages/day/cubit/day_page_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:planner_app/src/core/services/isar_service.dart';
@@ -28,6 +30,27 @@ class DayPageCubit extends Cubit<DayPageState> {
     );
 
     emit(state.copyWith(dayEvents: filteredEvents, dbStatus: DbStatus.loaded));
+  }
+
+  void search(String text) {
+    emit(state.copyWith(dbStatus: DbStatus.searching));
+    log(text);
+    final foundEvents = <Event>[];
+    if (text.trim().isNotEmpty) {
+      for (final event in state.dayEvents) {
+        if (event.description!.toLowerCase().contains(text) ||
+            event.title!.toLowerCase().contains(text)) {
+          foundEvents.add(event);
+        }
+        emit(state.copyWith(
+            foundEvents: foundEvents,
+            dbStatus:
+                foundEvents.isNotEmpty ? DbStatus.found : DbStatus.notFound));
+      }
+    } else {
+      emit(state.copyWith(dbStatus: DbStatus.loaded));
+    }
+    log(state.dbStatus.toString());
   }
 
   Future<void> deleteEvent(Event event) async {
--- lib/src/modules/pages/day/day_page.dart
+++ lib/src/modules/pages/day/day_page.dart
@@ -2,6 +2,10 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:planner_app/injector.dart';
+import 'package:planner_app/src/core/services/isar_service.dart';
+import 'package:planner_app/src/core/utils/constants.dart';
+import 'package:planner_app/src/domain/entities/event.dart';
+import 'package:planner_app/src/modules/cubits/cubit/main_cubit.dart';
 import 'package:planner_app/src/modules/pages/day/cubit/day_page_cubit.dart';
 import 'package:planner_app/src/modules/widgets/event_tile.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -15,31 +19,88 @@ class DayPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<DayPageCubit>(
-      create: (context) => sl<DayPageCubit>()..getDayEvents(),
-      child: BlocConsumer<DayPageCubit, DayPageState>(
-        listenWhen: (previous, current) =>
-            previous.dbStatus != current.dbStatus,
-        listener: (context, state) {},
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<DayPageCubit>(
+          create: (context) => sl<DayPageCubit>()..getDayEvents(),
+        ),
+        BlocProvider<MainCubit>(
+          create: (context) => sl<MainCubit>(),
+        ),
+      ],
+      child: BlocBuilder<MainCubit, MainState>(
         builder: (context, state) {
-          return CustomScaffold(
-            appBarTitle: AppLocalizations.of(context)!.today,
-            body: ListView.builder(
-              itemCount: state.dayEvents.length,
+          return BlocConsumer<DayPageCubit, DayPageState>(
+            listenWhen: (previous, current) =>
+                previous.dbStatus != current.dbStatus,
+            listener: (context, state) {},
+            builder: (context, state) {
+              final currentList = state.dbStatus == DbStatus.found
+                  ? state.foundEvents
+                  : state.dayEvents;
+              return CustomScaffold(
+                appBarTitle: AppLocalizations.of(context)!.today,
+                body: Column(
+                  children: [
+                    if (context.read<MainCubit>().state.isSearchActive) ...[
+                      SearchBar(
+                        onChanged: (value) =>
+                            context.read<DayPageCubit>().search(value),
+                      )
+                    ],
+                    const SizedBox(height: Constants.padding10),
+                    _DayPageList(eventList: currentList),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _DayPageList extends StatelessWidget {
+  const _DayPageList({required this.eventList});
+
+  final List<Event> eventList;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<DayPageCubit, DayPageState>(
+      builder: (context, state) {
+        if (state.dbStatus == DbStatus.loading ||
+            state.dbStatus == DbStatus.searching) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state.dayEvents.isNotEmpty) {
+          if (state.dbStatus == DbStatus.notFound) {
+            return Center(child: Text(AppLocalizations.of(context)!.not_found));
+          }
+          return Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: eventList.length,
               itemBuilder: (context, index) => Padding(
                 padding: const EdgeInsets.only(bottom: 20),
                 child: EventTile(
-                  event: state.dayEvents[index],
+                  event: eventList[index],
                   onDismissed: () => context
                       .read<DayPageCubit>()
-                      .deleteEvent(state.dayEvents[index]),
+                      .deleteEvent(eventList[index]),
                   dayPageCubit: context.read<DayPageCubit>(),
                 ),
               ),
             ),
           );
-        },
-      ),
+        } else {
+          return Center(
+            child: Text(AppLocalizations.of(context)!.you_have_no_events),
+          );
+        }
+      },
     );
   }
 }
